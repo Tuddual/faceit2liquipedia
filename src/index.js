94710d4da4609f59d56b21076e5624ab8aa8ff4b
tud <tudual.quinio@protonmail.com>
2021-01-24 22:20:51 +0100

    added link verification and a message if bad req

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ btn_paste.addEventListener('click', async () => {
         const link = await navigator.clipboard.readText();
         const input = document.querySelector('input');
         input.value = link;
+        verif(link);
     } else console.log('Clipboard API unavailable');
 });
 
@@ -19,3 +20,33 @@ btn_copy.addEventListener('click', async () => {
         await navigator.clipboard.writeText(code);
     } else console.log('Clipboard API unavailable');
 });
+
+// Input
+const input = document.querySelector('.searchbar');
+
+input.addEventListener('input', async () => {
+    verif(input.value)
+});
+
+function verif (link) {
+
+    const input = document.querySelector('.input');
+
+    // Delete the old message
+    const oldmsg = document.querySelector('.result');
+    if (oldmsg) {
+        input.removeChild(oldmsg);
+    }
+
+    if (/^https:\/\/www\.faceit\.com\/[a-z]{2}\/championship\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/.test(link)) {
+        const msg = document.createElement('p');
+        msg.className = "result good";
+        msg.textContent = "Processing...";
+        input.appendChild(msg);
+    } else {
+        const msg = document.createElement('p');
+        msg.className = "result bad";
+        msg.textContent = "There is a mistake in the link.";
+        input.appendChild(msg);
+    }
+}
